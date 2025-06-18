f78d1d86f8f7d50f231c38f5ccc3ecd5b78cf0c7
Username643346723 <fludistvud@ya.ru>
2025-06-18 23:18:39 +0300

    add import table

--- bot/models/__init__.py
+++ bot/models/__init__.py
@@ -1,10 +1,20 @@
 __all__ = (
     "Base",
     "db_helper",
+    "User",
+    "BotToken",
 )
 
 from .base import Base
 
 from .db_helper import (
     db_helper,
+)
+
+from .users import (
+    User,
+)
+
+from .bot_token import (
+    BotToken,
 )
