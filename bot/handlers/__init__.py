f96abbbd05614fc56637221aa6e55c9685eec4ed
Ruslan <fludistvud@ya.ru>
2025-06-19 10:34:35 +0300

    add router admin

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 from .start import router as router_start
 from .bot_token import router as router_token
+from .admin import router as router_admin
 
 router = Router()
 
 router.include_router(router_start)
 router.include_router(router_token)
+router.include_router(router_admin)
