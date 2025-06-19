68ea8d6b92f2f9651bbb93c8286ec9b90e622455
Ruslan <fludistvud@ya.ru>
2025-06-19 15:27:40 +0300

    add router generate username

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,9 +2,11 @@ from aiogram import Router
 from .start import router as router_start
 from .bot_token import router as router_token
 from .admin import router as router_admin
+from .bot_username import router as router_username
 
 router = Router()
 
 router.include_router(router_start)
 router.include_router(router_token)
 router.include_router(router_admin)
+router.include_router(router_username)
