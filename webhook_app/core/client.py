ab156e35feb8f582ac6aa354970e2d8b3aa18985
Username643346723 <fludistvud@ya.ru>
2025-06-22 22:59:05 +0300

    added bot and dispatcher for aiogram

--- webhook_app/core/client.py
+++ webhook_app/core/client.py
@@ -1,3 +1,6 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from fastapi import FastAPI
 
 from webhook_app.core.config import settings
@@ -7,7 +10,13 @@ logger = setup_logger(__name__)
 
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
 
+bot = Bot(
+    token=settings.tg.token,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
+dp = Dispatcher()
+
 # Логируем информацию о запуске приложения
 
 logger.info("Application has started")
-
