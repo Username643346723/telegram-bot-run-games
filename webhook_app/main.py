dff1ca8f1979e0f2e491fa6dcf24fddc6e747603
Username643346723 <fludistvud@ya.ru>
2025-06-22 23:01:01 +0300

    added set webhook and host, port for uvicorn

--- webhook_app/main.py
+++ webhook_app/main.py
@@ -1,16 +1,17 @@
+import uvicorn
+
 from webhook_app.api import main_router
-from webhook_app.core.client import app
+from webhook_app.core.client import app, bot
+from webhook_app.core.config import settings
 
 app.include_router(main_router, prefix="/api/v1")
 
 
-# healthcheck (опционально)
-@app.get("/")
-async def root():
-    return {"status": "ok"}
+@app.on_event("startup")
+async def startup():
+    if settings.ENV == "development":
+        await bot.set_webhook(url=f"{settings.WEBHOOK_BASE_URL}/webhook/{settings.tg.webhook_id_base_bot}")
 
 
 if __name__ == '__main__':
-    import uvicorn
-
-    uvicorn.run(app)
+    uvicorn.run(app, host=settings.gunicorn.host, port=settings.gunicorn.port)
