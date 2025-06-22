c88b1effda39e68f8f6e43e78b9112e94938120b
Username643346723 <fludistvud@ya.ru>
2025-06-22 21:11:54 +0300

    add config with dotenv support

---
+++ webhook_app/core/config.py
@@ -0,0 +1,69 @@
+from typing import Literal
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, Field
+
+LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+
+
+class GunicornConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+    workers: int = 1
+    timeout: int = 900
+
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        'debug',
+        'info',
+        'warning',
+        'error',
+        'critical',
+    ] = 'info'
+    log_format: str = LOG_DEFAULT_FORMAT
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+    is_now_updated_db: bool = False
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+
+    APP_NAME: str = Field(env="APP_NAME", default="FastAPI Application")
+    DEBUG: bool = Field(env="DEBUG", default=False)
+    run: RunConfig = RunConfig()
+    gunicorn: GunicornConfig = GunicornConfig()
+    db: DatabaseConfig
+    logging: LoggingConfig = LoggingConfig()
+
+    WEBHOOK_SECRET: str
+    DOMAIN: str = "http://127.0.0.1:8000"
+
+
+# Экземпляр настроек
+settings = Settings()
