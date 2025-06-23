bfef537d59f8351f9b9462be07789286baa6be85
Username643346723 <fludistvud@ya.ru>
2025-06-23 20:10:39 +0300

    edit bot_id to webhook_id in func telegram_webhook

--- webhook_app/api/v1/endpoints/webhook.py
+++ webhook_app/api/v1/endpoints/webhook.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Request, HTTPException
 from webhook_app.core.config import settings
 from webhook_app.services.webhook_handler import handle_telegram_update
 
+
 router = APIRouter()
 
 
 @router.post("/webhook/{webhook_id}")
 async def telegram_webhook(
-        bot_id: str,
+        webhook_id: str,
         request: Request,
         secret: str
 ):
@@ -15,5 +16,5 @@ async def telegram_webhook(
         raise HTTPException(status_code=403, detail="Forbidden")
 
     data = await request.json()
-    await handle_telegram_update(bot_id=bot_id, update_data=data)
+    await handle_telegram_update(bot_id=webhook_id, update_data=data)
     return {"ok": True}
