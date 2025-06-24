42bddd183125248e4249b9d223dba00c1d8435e2
Ruslan <fludistvud@ya.ru>
2025-06-24 14:12:20 +0300

    .

---
+++ bot/main.py
@@ -0,0 +1,38 @@
+from aiogram import Dispatcher
+from core.bot import bot  # Импорт из core/bot.py
+from core.config import settings
+from libs.logging import setup_logger
+from bot.handlers import router_main  # Локальный импорт
+
+logger = setup_logger(__name__)
+
+
+def setup_dispatcher() -> Dispatcher:
+    """Инициализация и настройка диспетчера"""
+    dp = Dispatcher()
+    dp.include_router(router_main)
+    return dp
+
+
+async def main():
+    dp = setup_dispatcher()
+
+    logger.info(
+        f"Starting bot in polling mode...\n"
+        f"Environment: {settings.ENV}\n"
+        f"Bot ID: {bot.id}"
+    )
+
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logger.critical(f"Bot crashed: {e}")
+        raise
+    finally:
+        logger.info("Bot stopped")
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
