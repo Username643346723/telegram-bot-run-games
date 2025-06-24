bdb733a9e5320dd8d3b4305943d66cf5e9456b18
Ruslan <fludistvud@ya.ru>
2025-06-24 15:37:27 +0300

    adit admin_id -> admin_ids

--- core/config/app.py
+++ core/config/app.py
@@ -17,7 +17,7 @@ class Paths:
 
 class Telegram(BaseModel):
     token: str = Field(..., description="Token telegram bot")
-    admins_id: list[int] = Field(..., description="Admins ID")
+    admin_ids: list[int] = Field(..., description="Admins ID")
     webhook_id_base_bot: str = Field(..., )
 
 
