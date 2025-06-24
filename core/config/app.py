7bd9fddc237762a444d1a3f4597fe12838b7351b
Username643346723 <fludistvud@ya.ru>
2025-06-24 18:57:48 +0300

    replaced the definition of BASE_DIR

--- core/config/app.py
+++ core/config/app.py
@@ -6,7 +6,7 @@ from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path.cwd()
 
 
 class Paths:
