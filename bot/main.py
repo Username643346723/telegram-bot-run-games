987e491aa6fdc1fe95bd5ff50185fef701eb0ef6
Ruslan <fludistvud@ya.ru>
2025-06-24 14:29:17 +0300

    remove setup_dispatcher. Use import dp from  core.bot

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,5 @@
 from aiogram import Dispatcher
-from core.bot import bot  # Импорт из core/bot.py
+from core.bot import bot, dp  # Импорт из core/bot.py
 from core.config import settings
 from libs.logging import setup_logger
 from bot.handlers import router_main  # Локальный импорт
@@ -7,15 +7,8 @@ from bot.handlers import router_main  # Локальный импорт
 logger = setup_logger(__name__)
 
 
-def setup_dispatcher() -> Dispatcher:
-    """Инициализация и настройка диспетчера"""
-    dp = Dispatcher()
-    dp.include_router(router_main)
-    return dp
-
-
 async def main():
-    dp = setup_dispatcher()
+    dp.include_router(router_main)
 
     logger.info(
         f"Starting bot in polling mode...\n"
