7060b93e493b1f02416256fca9578085536f17dd
Ruslan <fludistvud@ya.ru>
2025-06-25 11:26:39 +0300

    Fixed logging for the main bot. If the webhook ID is not the one specified in the settings, then we write an error in the logs and do not execute the code Added a router for bots that were added by other users

--- webhook/api/v1/endpoints/webhook.py
+++ webhook/api/v1/endpoints/webhook.py
@@ -1,28 +1,44 @@
-from aiogram.types import Update
 from fastapi import APIRouter, Request
 
-from core.bot import dp, bot
+from core.config import settings
 from libs.logging.logger import setup_logger
+from webhook.services.webhook_handler import (
+    handle_telegram_update_base_bot,
+    handle_telegram_update_other_bot
+)
 
 logger = setup_logger(__name__)
 router = APIRouter()
 
 
 @router.post("/webhook/{webhook_id}")
-async def telegram_webhook(
+async def telegram_webhook_base_bot(
         webhook_id: str,
         request: Request,
 ):
-    logger.info(f"webhook_id: {webhook_id}, request: {str(request)}")
-    body = await request.body()
-    logger.info(f"Raw body: {body}")
-    # if secret != settings.WEBHOOK_SECRET:
-    #     raise HTTPException(status_code=403, detail="Forbidden")
+    if settings.tg.webhook_id_base_bot != webhook_id:
+        body = await request.body()
+
+        logger.error(f"Webhook_id основного бота, "
+                     f"который был передан телеграмм не "
+                     f"соответствует установленному в настройках. "
+                     f"Webhook_id: {webhook_id}, Request body: {str(body)}")
+        return {"ok": True}
 
     json_data = await request.json()
-    update = Update(**json_data)
 
-    await dp.feed_webhook_update(bot, update)
+    await handle_telegram_update_base_bot(webhook_id, request_json_data=json_data)
 
-    # await handle_telegram_update(bot_id=webhook_id, update_data=data)
+    return {"ok": True}
+
+
+@router.post("/other_bot/webhook/{webhook_id}")
+async def telegram_webhook_other_bot(
+        webhook_id: str,
+        request: Request,
+):
+    json_data = await request.json()
+
+    await handle_telegram_update_other_bot(webhook_id, request_json_data=json_data)
+
     return {"ok": True}
