2802c76d7bff443c2d29bd4d316bfd78be03177a
Ruslan <fludistvud@ya.ru>
2025-06-25 12:16:31 +0300

    added storage of bot class instances

--- core/bot/__init__.py
+++ core/bot/__init__.py
@@ -12,3 +12,5 @@ main_bot = Bot(
 main_bot_dp = Dispatcher()  # Основной диспетчер, который отвечает работу основного бота
 
 user_bots_dp = Dispatcher()  # Диспетчер отвечающий за ботов добавленных пользователями
+
+active_user_bots: dict[str, Bot] = {}
